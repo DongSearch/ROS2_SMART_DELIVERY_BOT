7943baf79285b6977aed15b5b7dbc9eeaa74635a
DongSearch <gd.baek1495@gmail.com>
2026-04-06 17:57:37 +0900

    edit rqt v0.6

--- my_rqt_plugin/my_rqt_plugin/robot_qt.py
+++ my_rqt_plugin/my_rqt_plugin/robot_qt.py
@@ -7,8 +7,12 @@ from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget
 from PyQt5.QtCore import pyqtSignal, QObject
 from dr_interfaces.action import MoveTo
-from dr_interfaces.srv import Resume, EmergerncyStop
+from dr_interfaces.srv import Resume, EmergencyStop
 import math
+import threading
+from ament_index_python.packages import get_package_share_directory
+
+
 class Bridge(QObject):
     feedback_signal = pyqtSignal(float, float, float)
     log_signal = pyqtSignal(str)
@@ -18,30 +22,60 @@ class ControlPlugin(Plugin):
         super().__init__(context)
         self.setObjectName("ControlPlugin")
         self.widget = QWidget()
-        ui_file = os.path.join(os.path.dirname(__file__), 'resource/robot_qt.ui')
+        pkg_path = get_package_share_directory('my_rqt_plugin')
+        ui_file = os.path.join(pkg_path,'resource','resource/robot_qt.ui')
         loadUi(ui_file,self.widget)
         context.add_widget(self.widget)
         self.bridge = Bridge()
+        rclpy.init()
         self.node = ControlNode(self.bridge)
+        self.initial_distance = None
+        self.target_x = None
+        self.target_y = None
         self.widget.btn_send.clicked.connect(self.on_send_goal)
         self.bridge.feedback_signal.connect(self.update_feedback)
         self.widget.btn_emg.clicked.connect(self.node.send_emergency)
         self.widget.btn_re.clicked.connect(self.node.send_resume)
+        # self.widget.rbn_res.clicked.connect(self.node.send_addition)
+        # self.widget.rbn_pre.clicked.connect(self.node.send_addition)
+        # self.widget.rbn_du.clicked.connect(self.node.send_addition)
+        # self.widget.rbn_qu.clicked.connect(self.node.send_addition)
+        self.widget.qt_plot.setXRange(0, 100)
+        self.widget.qt_plot.setYRange(0, 100)
+        self.widget.qt_plot.setLabel('left', 'Y')
+        self.widget.qt_plot.setLabel('bottom', 'X')
+        self.widget.qt_plot.showGrid(x=True, y=True)
+        self.bridge.log_signal.connect(self.update_log)
+
+        threading.Thread(target=rclpy.spin,args=(self.node,), daemon=True).start()
+    
+    def update_log(self,msg):
+        self.widget.tb_error.append(msg)
+
         
     def on_send_goal(self):
+        self.widget.prb.setValue(0)
         x = float(self.widget.le_x.text())
         y = float(self.widget.le_y.text())
         self.node.send_goal(x,y)
-        self.widget.lcd_tx = self.widget.le_x.text()
-        self.widget.lcd_ty = self.widget.le_y.text()
+        self.target_x = x
+        self.target_y = y
+        self.initial_distance = None
+        self.widget.lcd_tx.display(x)
+        self.widget.lcd_ty.display(y)
         
-        
-        self.initial_distance = math.sqrt((self.widget.lcd_cx.display(x)-self.widget.lcd_tx)^2 + (self.widget.lcd_cy.display(y)-self.widget.lcd_ty)^2)
+    
     
     def update_feedback(self,x,y,dist):
         self.widget.lcd_cx.display(x)
         self.widget.lcd_cy.display(y)
-        self.widget.prb.display(dist/self.initial_distance)
+        if self.initial_distance is None and dist is not None:
+            self.initial_distance = dist
+
+        if self.initial_distance:
+            progress = 1 - (dist/self.initial_distance)
+            progress = max(0.0,min(1.0,progress)) # 0~1 clamping
+            self.widget.prb.setValue(int(progress * 100))
 
 
 
@@ -52,7 +86,7 @@ class ControlNode(Node):
         # Action Client
         self.action_client = ActionClient(self, MoveTo,"moveto")
         self.cli_resume = self.create_client(Resume,'resume')
-        self.cli_emr = self.create_client(Resume,'emergency')
+        self.cli_emr = self.create_client(EmergencyStop,'emergency')
         self.current_goal_handle = None
 
     def send_goal(self,x,y):
@@ -72,7 +106,7 @@ class ControlNode(Node):
     def goal_response_callback(self, future):
         goal_handle = future.result()
         if not goal_handle.accepted:
-            self.bridge.log_signal.emit("[WARM] Goal Rejected")
+            self.bridge.log_signal.emit("[WARN] Goal Rejected")
             return
         
         self.current_goal_handle = goal_handle
@@ -108,7 +142,7 @@ class ControlNode(Node):
         if not self.cli_emr.wait_for_service(1.0):
             self.bridge.log_signal.emit("[WARN] Wait service for Emergency")
             return
-        req = EmergerncyStop.Request()
+        req = EmergencyStop.Request()
         future = self.cli_emr.call_async(req)
         future.add_done_callback(self.emergency_callback)
     
